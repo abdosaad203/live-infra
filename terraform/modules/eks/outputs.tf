42dcb1e4be305901950c3b9c5f48797a455dea8b
abdosaad203 <abdosaad4900@gmail.com>
2026-06-06 14:13:01 +0300

    add addons

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -5,3 +5,11 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = aws_eks_cluster.this.endpoint
 }
+
+output "cluster_ca" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+output "oidc_issuer" {
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
